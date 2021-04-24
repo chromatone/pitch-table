4dfd09514f770b7c6861de51c6e98c443ffe8543
davay42 <fond@fondfrukt.ru>
2021-04-24 20:42:59 +0300

    gestures

--- src/components/table/cell.vue
+++ src/components/table/cell.vue
@@ -1,11 +1,28 @@
 <template lang="pug">
-.cell(:style="{ backgroundColor: color, color: textColor }")
+.cell(
+  v-drag="dragHandler",
+  :drag-options=`{
+    filterTaps: true
+  }`,
+  :style=`{
+    backgroundColor: color,
+    color: textColor
+  }`, 
+  :class="{ active }")
+  .absolute.w-full.h-full.top-0.left-0.bottom-0
+    .absolute.border-t.bg-gray-700.bg-opacity-50.w-full.bottom-0(
+      :style="{ height: voice.vol + '%' }"
+    ) 
+  .absolute.h-full.top-0.left-0.right-0.text-center
+    .pan.absolute.bg-gray-100.bg-opacity-50.h-full.m-auto(
+      :style="{ left: voice.pan + '%' }"
+    ) 
   .letter.text-xl.px-1(v-if="state.show.letters") {{ note.name }}
   table-frequency(:pitch="note.pitch", :octave="octave")
 </template>
 
 <script setup>
-import { defineProps, computed, ref } from 'vue'
+import { defineProps, computed, ref, reactive } from 'vue'
 import { state } from '@store/state.js'
 const props = defineProps({
   note: Object,
@@ -15,7 +32,32 @@ const props = defineProps({
   }
 });
 
-const active = ref(false)
+const voice = reactive({
+  vol: 0,
+  pan: 50,
+})
+
+const active = computed(() => {
+  return voice.vol > 0
+})
+
+const dragHandler = (dragEvent) => {
+  let { movement: [x, y], dragging, tap } = dragEvent
+  if (!tap) {
+    voice.vol += -y / 20
+    if (voice.vol > 100) voice.vol = 100
+    if (voice.vol < 0) voice.vol = 0
+
+    voice.pan += x / 20
+
+    if (voice.pan > 100) voice.pan = 100
+    if (voice.pan < 0) voice.pan = 0
+  } else {
+    voice.vol = 0;
+    voice.pan = 50
+  }
+}
+
 
 const color = computed(() => {
   return (
@@ -45,9 +87,23 @@ const textColor = computed(() => {
 
 <style  scoped>
 .cell {
-  @apply flex flex-col py-4 flex-1 cursor-pointer select-none opacity-50 hover:opacity-100;
+  @apply relative border border flex flex-col py-4 flex-1 cursor-pointer select-none opacity-50 hover:opacity-100;
   transition: all 100ms ease;
   min-width: 2em;
   min-height: 2em;
+}
+.cell.active {
+  @apply opacity-90;
+}
+
+.volume {
+  width: 6px;
+  @apply flex items-end;
+}
+
+.pan {
+  left: 50%;
+  width: 1px;
+  transform: translateX(-2.5px);
 }
 </style>
