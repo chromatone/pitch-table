4982f6efec1d7a345507d943f6975d83755bb016
davay42 <fond@fondfrukt.ru>
2021-05-11 14:02:11 +0300

    deploy to pages

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-.bg-gray-100(class="dark:(bg-gray-700 text-gray-100)")
+.flex
   .flex.flex-col.w-full.flex-auto
     header-row
     control-row
@@ -17,16 +17,21 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   min-height: 100vh;
+  @apply bg-gray-100 dark:(bg-gray-700 text-gray-100);
+}
+
+body {
+  min-height: 100vh;
 }
 
 .scroll-bar {
-  @apply min-w-6;
-  background-color: #eee;
+  @apply min-w-6 bg-gray-200 dark:bg-gray-500;
   background-image: linear-gradient(
     0deg,
     transparent 80%,
     hsla(0, 0%, 0%, 0.2) 20%
   );
+  background-blend-mode: multiply;
   background-size: 20px 4px;
 }
 </style>
