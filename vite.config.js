9317e013bc4d88a9396c1c2eaf6bebfa690ae9c3
davay <davay@defucc.me>
2025-01-19 00:29:00 +0700

    feat: sw and manifest

--- vite.config.js
+++ vite.config.js
@@ -34,3 +34,30 @@ export default defineConfig({
     }),
   ],
 })
+
+
+function viteBuildScript() {
+  return {
+    name: 'vite-build-script',
+    transformIndexHtml(html) {
+      if (process.env.NODE_ENV === 'production') {
+        return html.replace(/<!-- Stats production build insert -->/, `<script async defer src="https://stats.chromatone.center/script.js" data-website-id="828fbb1d-0f47-4a05-886a-4b50cfe8ba02"></script>
+          
+          <script>
+    if ('serviceWorker' in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+          .then((registration) => {
+            console.log('Service Worker registered with scope: ', registration.scope);
+          })
+          .catch((error) => {
+            console.error('Service Worker registration failed: ', error);
+          });
+      });
+    }
+  </script>`);
+      }
+      return html;
+    },
+  };
+}
